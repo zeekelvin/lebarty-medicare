5bd686a048938fcd9741c5b462303dc52d84342f
zeekelvin <mikejgeorge32@gmail.com>
2026-08-21 21:59:23 -0400

    Add CI/CD pipeline for Cloudflare Workers

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -2,6 +2,12 @@ import type { MetadataRoute } from 'next';
 import { site } from '@/lib/site';
 
 export default function robots(): MetadataRoute.Robots {
+  if (process.env.NEXT_PUBLIC_SITE_ENV === 'staging') {
+    return {
+      rules: [{ userAgent: '*', disallow: '/' }],
+    };
+  }
+
   return {
     rules: [{ userAgent: '*', allow: '/' }],
     sitemap: `${site.url}/sitemap.xml`,
